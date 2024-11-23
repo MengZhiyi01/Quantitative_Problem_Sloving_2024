7c67caeca220351c89c1d86bff72ad42fe21e1e6
Meng_Zhiyi <testkarma@MacBook-Pro-89.local>
2024-11-23 15:06:14 +0800

    Update README.md and make some modifies

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # phbs-qps-2024
-This is Homework 1 of PHBS Quantitative Problem Solving 2024
+
+## Introduction
+This is Homework 1 of the PHBS Quantitative Problem Solving 2024, undertaken by **Zhiyi_Meng**.
+
+## Quick Start
+1. Python 3.10.13
+
+2. Install dependencies.
+- pandas==2.2.3
+- pandas_datareader==0.10.0
+
+3. Run scripts/calculation.py
+
+You can run the scripts *from the project root directory in the terminal*.
+```shell
+conda activate -your_enviroment_name
+
+cd /path/to/project_root #[your root to phbs-qps-2024]
+
+python -m scripts.calculation
+```
+
+4. All the processed data will be save in <code> data</code>, and **the last 4 quarters of inflation** will be printed out.
+
+## Dataset
+The downloaded CPI data is from Federal Reserve Economic Data (FRED) through pandas datareader
+
+- [FRED link](https://fred.stlouisfed.org/series/CPILFESL)
+
+- [Pandas_datareader link](https://pydata.github.io/pandas-datareader/stable/index.html)
--- scripts/calculation.py
+++ scripts/calculation.py
@@ -1,24 +1,40 @@
 ## This is just a test
 import pandas as pd
-import pandas_datareader.data as web
 from src.utils import *
 import os
 
 # Use the function in src.utils
-cpi_data = fetch_data_and_clean('CPIAUCSL', 'fred', start_date = '2015-01-01')
+def generate_inflation(name = 'CPIAUCSL', source = 'fred', end_date = datetime.now(), start_date = datetime.now() - timedelta(days=365*4), save = True):
+    cpi_data = fetch_data_and_clean('CPIAUCSL', 'fred', start_date = '2015-01-01')
 
-# Calculate the inflation rate for the last 4 quarters
-quarters = cpi_data.resample('QE').mean()
-inflation_rates = quarters.pct_change() * 100
-inflation_rates.dropna(inplace=True)
+    # Calculate the inflation rate for the last 4 quarters
+    quarters = cpi_data.resample('QE').mean()
+    inflation_rates = quarters.pct_change() * 100
 
-# Print the last 4 quarters of inflation
-print(f'The last 4 quarters of inflation are:\n{inflation_rates.tail(4)}')
+    # Rename the columns
+    inflation_rates.rename(columns={'CPIAUCSL': 'Inflation_rate'},inplace=True)
+    inflation_rates.dropna(inplace=True)
 
-# Create the 'data' directory if it does not exist
-data_dir = '../data'
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
+    # Convert the date format to quarters
+    inflation_rates['Date'] = inflation_rates.index.to_period('Q')
+    inflation_rates.reset_index(inplace=True)
+    inflation_rates = inflation_rates[['Date', 'Inflation_rate']]
 
-# Save the inflation rates to a CSV file
-inflation_rates.to_csv(f'{data_dir}/inflation_rates.csv')
+    # Create the 'data' directory if it does not exist
+    if save:
+        data_dir = '../data'
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+
+        # Save the inflation rates to a CSV file
+        inflation_rates.to_csv(f'{data_dir}/inflation_rates.csv')
+
+    return inflation_rates
+
+if __name__=="__main__":
+    # Print the last 4 quarters of inflation
+    inflation_rates = generate_inflation()
+    print(f'The last 4 quarters of inflation are:\n{inflation_rates.tail(4)}')
+
+    # Print all cpi data
+    print(f'The all the inflation rates are:\n{inflation_rates}')
